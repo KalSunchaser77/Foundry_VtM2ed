e704d1f6d220e2a4eacd54b299a4227e572f230c
KalSunchaser77 <kalsun77@me.com>
2025-12-07 21:43:13 -0500

    Update exalted-actor-sheet.js

--- module/actor/template/exalted-actor-sheet.js
+++ module/actor/template/exalted-actor-sheet.js
@@ -8,7 +8,7 @@ export default class ExaltedActorSheet extends MortalActorSheet {
 	static get defaultOptions() {
 		return foundry.utils.mergeObject(super.defaultOptions, {
 			classes: ["wod20 wod-sheet exalted"],
-			template: "systems/worldofdarkness/templates/actor/exalted-sheet.html"
+			template: "systems/vtm20-2e-saa/templates/actor/exalted-sheet.html"
 		});
 	}
   
@@ -30,7 +30,7 @@ export default class ExaltedActorSheet extends MortalActorSheet {
 
 	/** @override */
 	get template() {
-		return "systems/worldofdarkness/templates/actor/exalted-sheet.html";
+		return "systems/vtm20-2e-saa/templates/actor/exalted-sheet.html";
 	}
 	
 	/** @override */
