8943a7256928d97752b5ef53edfd8c1149de275b
KalSunchaser77 <kalsun77@me.com>
2025-12-07 22:16:31 -0500

    Update wod-item-base.js

--- module/items/data/wod-item-base.js
+++ module/items/data/wod-item-base.js
@@ -61,7 +61,7 @@ export class WoDItem extends Item {
 			if (updateData.type == "Power") {
 				const imgUrl = _getImage(updateData);
 
-				if (updateData.img.startsWith('systems/worldofdarkness/')) {
+				if (updateData.img.startsWith('systems/vtm20-2e-saa/')) {
 					updateData.img = imgUrl;
 					updated = true;
 				}            
@@ -94,11 +94,11 @@ export class WoDItem extends Item {
 
 function _getImage(item) {
 	if (item.type == "Armor") {
-		return "systems/worldofdarkness/assets/img/items/armor.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/armor.svg";
 	}
 
 	if (item.type == "Fetish") {
-		return "systems/worldofdarkness/assets/img/items/fetish.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/fetish.svg";
 	}
 
 	if (item.type == "Item") {
@@ -106,107 +106,107 @@ function _getImage(item) {
 	}
 
 	if ((item.type == "Melee Weapon") && (item.system.isnatural)) {
-		return "systems/worldofdarkness/assets/img/items/naturalweapons.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/naturalweapons.svg";
 	}
 
 	if ((item.type == "Melee Weapon") && (!item.system.isnatural)) {
-		return "systems/worldofdarkness/assets/img/items/meleeweapons.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/meleeweapons.svg";
 	}
 
 	if (item.type == "Ranged Weapon") {
-		return "systems/worldofdarkness/assets/img/items/rangedweapons.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/rangedweapons.svg";
 	}
 
 	if (item.type == "Feature") {
-		return "systems/worldofdarkness/assets/img/items/feature.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/feature.svg";
 	}
 
 	if (item.type == "Experience") {
-		return "systems/worldofdarkness/assets/img/items/feature.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/feature.svg";
 	}
 
 	if (item.type == "Power") {
 		if ((item.system.type == "wod.types.discipline") || (item.system.type == "wod.types.disciplinepath")) {
-			return "systems/worldofdarkness/assets/img/items/mainpower_vampire.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/mainpower_vampire.svg";
 		}
 
 		if ((item.system.type == "wod.types.disciplinepower") || (item.system.type == "wod.types.disciplinepathpower") || (item.system.type == "wod.types.combination")) {
-			return "systems/worldofdarkness/assets/img/items/power_vampire.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_vampire.svg";
 		}
 
 		if ((item.system.type == "wod.types.ritual") && (item.system.game == "vampire")) {
-			return "systems/worldofdarkness/assets/img/items/ritual_vampire.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/ritual_vampire.svg";
 		}
 
 		if (item.system.type == "wod.types.art") {
-			return "systems/worldofdarkness/assets/img/items/mainpower_changeling.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/mainpower_changeling.svg";
 		}
 
 		if (item.system.type == "wod.types.artpower") {
-			return "systems/worldofdarkness/assets/img/items/power_changeling.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_changeling.svg";
 		}
 
 		if (item.system.type == "wod.types.edge") {
-			return "systems/worldofdarkness/assets/img/items/mainpower_hunter.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/mainpower_hunter.svg";
 		}
 
 		if (item.system.type == "wod.types.edgepower") {
-			return "systems/worldofdarkness/assets/img/items/power_hunter.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_hunter.svg";
 		}
 
 		if (item.system.type == "wod.types.lore") {
-			return "systems/worldofdarkness/assets/img/items/mainpower_demon.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/mainpower_demon.svg";
 		}
 
 		if (item.system.type == "wod.types.lorepower") {
-			return "systems/worldofdarkness/assets/img/items/power_demon.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_demon.svg";
 		}
 
 		if ((item.system.type == "wod.types.arcanoi")||(item.system.type == "wod.types.stain")||(item.system.type == "wod.types.horror")) {
-			return "systems/worldofdarkness/assets/img/items/mainpower_wraith.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/mainpower_wraith.svg";
 		}
 
 		if (item.system.type == "wod.types.arcanoipower") {
-			return "systems/worldofdarkness/assets/img/items/power_wraith.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_wraith.svg";
 		}
 
 		if (item.system.type == "wod.types.hekau") {
-			return "systems/worldofdarkness/assets/img/items/mainpower_mummy.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/mainpower_mummy.svg";
 		}
 
 		if (item.system.type == "wod.types.hekaupower") {
-			return "systems/worldofdarkness/assets/img/items/power_mummy.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_mummy.svg";
 		}
 
 		if ((item.system.type == "wod.types.exaltedcharm") || (item.system.type == "wod.types.exaltedsorcery")) {
-			return "systems/worldofdarkness/assets/img/items/power_exalted.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_exalted.svg";
 		}
 
 		if (item.system.type == "wod.types.numina") {
-			return "systems/worldofdarkness/assets/img/items/mainpower_mage.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/mainpower_mage.svg";
 		}
 
 		if (item.system.type == "wod.types.numinapower") {
-			return "systems/worldofdarkness/assets/img/items/power_mage.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_mage.svg";
 		}
 
 		if ((item.system.type == "wod.types.ritual") && (item.system.game == "demon")) {
-			return "systems/worldofdarkness/assets/img/items/ritual_demon.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/ritual_demon.svg";
 		}
 
 		if (item.system.type == "wod.types.gift") {
-			return "systems/worldofdarkness/assets/img/items/power_werewolf.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/power_werewolf.svg";
 		}
 
 		if (item.system.type == "wod.types.rite") {
-			return "systems/worldofdarkness/assets/img/items/ritual_werewolf.svg";
+			return "systems/vtm20-2e-saa/assets/img/items/ritual_werewolf.svg";
 		}
 
-		return "systems/worldofdarkness/assets/img/items/power.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/power.svg";
 	}
 
 	if (item.type == "Rote") {
-		return "systems/worldofdarkness/assets/img/items/rote_mage.svg";
+		return "systems/vtm20-2e-saa/assets/img/items/rote_mage.svg";
 	}
 
 	return "";
